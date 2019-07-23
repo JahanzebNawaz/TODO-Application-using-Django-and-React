861131d9cf7a8131d9753c5073fab0a8f11d98c1
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-07-23 23:48:23 +0500

    Django React Todo

--- frontendproject/src/App.js
+++ frontendproject/src/App.js
@@ -25,13 +25,71 @@ class App extends Component {
       viewCompleted: false,
       todoList: TodoItems,
     };
-    
   }
-  render(){
-    return(
-      <h1>Welcome To Django React </h1>
-    );
-  }
-}
 
+    // changing state of viewCompleted 
+  displayCompleted = status => {
+    if(status){
+      return this.setState({ viewCompleted: true});
+    }
+    else{
+      return this.setState({ viewCompleted: false });
+    }
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span onCLick={() => this.displayCompleted(true)} className={this.state.viewCompleted ? "active": ""}> Complete </span>
+        <span onClick={() => this.displayCompleted(false)} className={this.state.viewCompleted ? "":"active"}> Incomplete </span>
+      </div>
+    );
+  };
+
+
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+      item => item.completed = viewCompleted,
+    );
+
+    return newItems.map(item => (
+      <li key={item.id}> {item.title} </li>
+
+      
+      // <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+      //   <span className={'todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""} '} title={item.description}> {item.title} </span>
+      //   <span>
+      //     <button className="btn btn-secondary mr-2" > Edit </button>
+      //     <button className="btn btn-danger"> Delete </button>
+      //   </span>
+      // </li>
+    ));
+  };
+
+  render () {
+    return (
+      <section className="content">
+        <h1 className="text-white text-uppercase text-center my-4">ToDo App</h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+              <div className="card p-3">
+                  <div className="">
+                    <button className="btn btn-primary">Add task</button>
+                  </div>
+
+                  {/* adding render table list here */}
+                  {this.renderTabList() }
+
+                  <ul className="list-group list-group-flush">
+                    {this.renderItems() }
+                  </ul>
+              </div>
+          </div>
+        </div>
+      </section>
+
+    );
+  };
+}
 export default App;

08494c5497f7f6b7be382354efdfb9140a2a7bb6
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-07-24 13:03:38 +0500

    Django React

--- frontendproject/src/App.js
+++ frontendproject/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      viewCompleted: false,
+      viewCompleted: true,
       todoList: TodoItems,
     };
   }
@@ -53,17 +53,14 @@ class App extends Component {
       item => item.completed = viewCompleted,
     );
 
-    return newItems.map(item => (
-      <li key={item.id}> {item.title} </li>
-
-      
-      // <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
-      //   <span className={'todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""} '} title={item.description}> {item.title} </span>
-      //   <span>
-      //     <button className="btn btn-secondary mr-2" > Edit </button>
-      //     <button className="btn btn-danger"> Delete </button>
-      //   </span>
-      // </li>
+    return newItems.map(item => (     
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+        <span className={'todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""} '} title={item.description}> {item.title} </span>
+        <span>
+          <button className="btn btn-secondary mr-2" > Edit </button>
+          <button className="btn btn-danger"> Delete </button>
+        </span>
+      </li>
     ));
   };
 

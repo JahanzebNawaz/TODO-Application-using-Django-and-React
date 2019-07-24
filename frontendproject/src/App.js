226eab51d4e296448de55407a34cee0c7514f024
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-07-24 15:47:37 +0500

    Django React Together

--- frontendproject/src/App.js
+++ frontendproject/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Modal from './components/Modal';
 
 
 // add const variable to add demo data
@@ -13,7 +14,7 @@ const TodoItems = [
     id: 2,
     title: "Demo Title Two",
     description: "Welcome to Django React Two Demo Description",
-    completed: true,
+    completed: false,
   }
 ];
 
@@ -22,10 +23,35 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      modal: false,
       viewCompleted: true,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false,
+      },
       todoList: TodoItems,
     };
   }
+
+  toggle = () => {
+        this.setState({ modal: !this.state.modal });
+      };
+
+  handleSubmit = item => {
+    this.toggle();
+    alert("save" + JSON.stringify(item));
+  };
+  handleDelete = item => {
+    alert("delete" + JSON.stringify(item));
+  };
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
 
     // changing state of viewCompleted 
   displayCompleted = status => {
@@ -40,7 +66,7 @@ class App extends Component {
   renderTabList = () => {
     return (
       <div className="my-5 tab-list">
-        <span onCLick={() => this.displayCompleted(true)} className={this.state.viewCompleted ? "active": ""}> Complete </span>
+        <span onCLick={() => this.displayCompleted(true)} className={this.state.viewCompleted ? "active":""}> Complete </span>
         <span onClick={() => this.displayCompleted(false)} className={this.state.viewCompleted ? "":"active"}> Incomplete </span>
       </div>
     );
@@ -57,8 +83,8 @@ class App extends Component {
       <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
         <span className={'todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""} '} title={item.description}> {item.title} </span>
         <span>
-          <button className="btn btn-secondary mr-2" > Edit </button>
-          <button className="btn btn-danger"> Delete </button>
+          <button onClick={() => this.editItem(item)} className="btn btn-secondary mr-2" > Edit </button>
+          <button onClick={() => this.handleDelete(item)} className="btn btn-danger"> Delete </button>
         </span>
       </li>
     ));
@@ -72,18 +98,26 @@ class App extends Component {
           <div className="col-md-6 col-sm-10 mx-auto p-0">
               <div className="card p-3">
                   <div className="">
-                    <button className="btn btn-primary">Add task</button>
+                    <button onClick={this.createItem} className="btn btn-primary">Add task</button>
                   </div>
 
                   {/* adding render table list here */}
                   {this.renderTabList() }
 
                   <ul className="list-group list-group-flush">
+                    {/* list of TODO */}
                     {this.renderItems() }
                   </ul>
               </div>
           </div>
         </div>
+        {this.state.modal ? (
+              <Modal
+                activeItem={this.state.activeItem}
+                toggle={this.toggle}
+                onSave={this.handleSubmit}
+              />
+            ) : null}
       </section>
 
     );
